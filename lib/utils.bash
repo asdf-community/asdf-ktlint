fd72ee294d4c42be67bc6a3386af92227cce29e3
Ensar Sarajčić <es.ensar@gmail.com>
2021-11-16 14:31:03 +0100

    Update scripts to match ktlint convetions

--- lib/utils.bash
+++ lib/utils.bash
@@ -2,7 +2,6 @@
 
 set -euo pipefail
 
-# TODO: Ensure this is the correct GitHub homepage where releases can be downloaded for ktlint.
 GH_REPO="https://github.com/pinterest/ktlint"
 TOOL_NAME="ktlint"
 TOOL_TEST="ktlint --help"
@@ -31,8 +30,6 @@ list_github_tags() {
 }
 
 list_all_versions() {
-  # TODO: Adapt this. By default we simply list the tag names from GitHub releases.
-  # Change this function if ktlint has other means of determining installable versions.
   list_github_tags
 }
 
@@ -41,8 +38,7 @@ download_release() {
   version="$1"
   filename="$2"
 
-  # TODO: Adapt the release URL convention for ktlint
-  url="$GH_REPO/archive/v${version}.tar.gz"
+  url="$GH_REPO/releases/${version}/ktlint"
 
   echo "* Downloading $TOOL_NAME release $version..."
   curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
@@ -61,7 +57,6 @@ install_version() {
     mkdir -p "$install_path"
     cp -r "$ASDF_DOWNLOAD_PATH"/* "$install_path"
 
-    # TODO: Asert ktlint executable exists.
     local tool_cmd
     tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
     test -x "$install_path/bin/$tool_cmd" || fail "Expected $install_path/bin/$tool_cmd to be executable."
